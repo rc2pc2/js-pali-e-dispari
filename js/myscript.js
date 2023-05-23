d04e484b024b80372bd66330cc15fdb2564f3170
rc2pc2 <petriccarcc@gmail.com>
2023-05-23 10:23:07 +0200

    odd or even simple game implementation

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,43 @@
-const userWord = prompt('Type a word');
+// const userWord = prompt('Type a word');
 
-if (isPalindrome(userWord)){
-    console.log('The given word is palindrome');
+// if (isPalindrome(userWord)){
+//     console.log('The given word is palindrome');
+// } else {
+//     console.log('The given word is not palindrome');
+// }
+
+const userChoice = prompt('Pick even or odd');
+const userNumber = parseInt(prompt('Pick a number between 1 and 5'));
+
+const aINumber = getRandomInt(1,5);
+
+const sum = userNumber + aINumber;
+
+let result = "";
+if ( isEven(sum) ){
+    result = 'even';
 } else {
-    console.log('The given word is not palindrome');
+    result = 'odd';
+}
+
+if ( result === userChoice.toLowerCase()){
+    console.log('Player wins!', sum, aINumber, userNumber);
+} else {
+    console.log('I WON AHAHAHAHAHAHAHAHAHAHAHA!!! POWER TO THE MACHINES!' , sum, aINumber, userNumber);
+}
+
+
+/**
+ * Function that checks if a given number is even.
+ *
+ * @param number the number to be checked
+ * @returns true if the number is even, false otherwise.
+ */
+function isEven(number){
+    if ( number % 2 === 0){
+        return true;
+    }
+    return false;
 }
 
 /**
@@ -33,4 +67,17 @@ function wordReverser(wordToReverse){
     }
 
     return reversedWord;
+}
+
+/**
+ * Function that generates a random number (not secure) between two values, both included.
+ *
+ * @param minumNumber the included minium value of the random generated number range.
+ * @param maximumNumber the included maximum value of the random generated number range
+ * @returns A randomly generated number.
+ */
+function getRandomInt(minumNumber, maximumNumber){
+    const randomNumber = Math.floor( Math.random() * ( maximumNumber - minumNumber +1) + minumNumber);
+
+    return randomNumber;
 }
